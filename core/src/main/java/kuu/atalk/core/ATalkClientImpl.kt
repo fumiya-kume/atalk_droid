d5609fedbeb8dd9b54601ce4a84041d387235fc9
Fumiya Kume <fumiya.kume@hotmail.com>
2020-10-08 14:18:28 +0900

    add implementation

---
+++ core/src/main/java/kuu/atalk/core/ATalkClientImpl.kt
@@ -0,0 +1,48 @@
+package kuu.atalk.core
+
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioTrack
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ATalkClientImpl
+    (
+    private val baseUrl:String
+) : ATalkClient {
+    suspend fun say(content:String){
+        withContext(Dispatchers.IO){
+            val url = "${baseUrl}?$content"
+            val result = url.httpGet()
+                .useHttpCache(true)
+                .response()
+
+            result.third.fold(
+                success = {
+                    AudioTrack.Builder()
+                        .setAudioAttributes(
+                            AudioAttributes.Builder()
+                                .setUsage(AudioAttributes.USAGE_GAME)
+                                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                                .build()
+                        )
+                        .setAudioFormat(
+                            AudioFormat.Builder()
+                                .setEncoding(AudioFormat.ENCODING_PCM_8BIT)
+                                .setSampleRate(8000)
+                                .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                                .build()
+                        )
+                        .setBufferSizeInBytes(8000 * 2)
+                        .setTransferMode(AudioTrack.MODE_STATIC)
+                        .build()
+                        .play()
+                },
+                failure = {
+
+                }
+            )
+        }
+    }
+}
